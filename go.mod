d30d1d1fd844c0c62037b23ba75d2d9684d180f5
yanni <ycoroneos@gmail.com>
2020-06-05 21:15:43 -0700

    set our gpios to input pd

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ycoroneos/GhostInTheHotTub
 
 go 1.14
+
+require github.com/stianeikeland/go-rpio/v4 v4.4.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/stianeikeland/go-rpio/v4"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	fmt.Printf("hello\n")
+	err := rpio.Open()
+	check(err)
+
+	pins := []rpio.Pin{rpio.Pin(14), rpio.Pin(15), rpio.Pin(18), rpio.Pin(17), rpio.Pin(27), rpio.Pin(22)}
+
+	for _, pin := range pins {
+		pin.Input()
+		pin.PullDown()
+	}
 }
